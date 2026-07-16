71e2802d91ddf75ed534ac83a0906fd430e87988
shuiping233 <1944680304@qq.com>
2026-07-16 20:28:37 +0800

    新建 Avalonia 应用

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MeMeManager
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
